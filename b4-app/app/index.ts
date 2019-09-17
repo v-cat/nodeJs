365dc788024375a50ce63067c34ce5a7a82070a3
v-vat <1936881152@qq.com>
2019-09-17 19:42:16 +0800

    第八章：实现hash路由

--- b4-app/app/index.ts
+++ b4-app/app/index.ts
@@ -2,9 +2,24 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 // @ts-ignore
 import * as templates from './templates.ts'
-document.body.innerHTML=templates.main;
+document.body.innerHTML = templates.main({});
 const mainElement = document.body.querySelector('.b4-main');
-
 const alertsElement = document.body.querySelector('.b4-alerts');
-mainElement.innerHTML=templates.welcome;
-alertsElement.innerHTML=templates.alert
+
+// mainElement.innerHTML=templates.welcome();
+// alertsElement.innerHTML=templates.alert({
+//     type:'info',
+//     message:'Handlebars is working!!'
+// })
+const showView = async () => {
+    const [view, ...params] = window.location.hash.split('/');
+    switch (view) {
+        case "#welcome":
+            mainElement.innerHTML = templates.welcome({});
+            break;
+        default:
+            throw Error(`unrecognized view: ${view}`)
+    }
+}
+window.addEventListener('hashchange',showView)
+showView().catch(err=>(window.location.hash="#welcome"))
--- b4-app/app/templates.ts
+++ b4-app/app/templates.ts
@@ -1,26 +1,26 @@
-export const main = `
+import * as Handlebars from 'handlebars'
 
+export const main = Handlebars.compile(`
 <div class="container">
 <h1>B4-Book Bundler </h1>
 <div class="b4-alerts"></div>
 <div class="b4-main"></div>
 <p>${new Date()} </p>
 </div>
-`;
-export const welcome = `
+`);
+export const welcome =Handlebars.compile( `
 <div class="jumbotron">
 <h1>
 Welcome!
 </h1>
 <p> B4 is an application for creating book bundles.</p>
 </div>
-`;
-export const alert = `
-<div class='alert alert-success alert-dismissible  in' role='alert'>
+`);
+export const alert = Handlebars.compile(`
+<div class='alert alert-{{type}} alert-dismissible  in' role='alert'>
 <button class='close' data-dismiss='alert' aria-label='Close >
 <span aria-hidden='true' > &times;</span>
 </button>
-<strong> Success!</strong>
-Bootstrap is working!
+{{message}}
 </div>
-`
+`)
